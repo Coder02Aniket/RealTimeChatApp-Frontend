01f9a6e020deb31e5c469e3df89a6103b2345115
Aniket Shankar Nangare <94744799+Coder02Aniket@users.noreply.github.com>
2024-12-16 13:01:53 +0530

    Update AxiosHelper.js

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "https://20.255.48.137:8080";
+export const baseURL = "https://20.255.48.137";
 export const httpClient = axios.create({
   baseURL: baseURL,
 
