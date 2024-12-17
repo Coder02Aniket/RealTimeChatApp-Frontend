425149fee929334d800b5527f5dd9bea1f18e69d
Aniket Shankar Nangare <94744799+Coder02Aniket@users.noreply.github.com>
2024-12-17 12:27:56 +0530

    Update AxiosHelper.js

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "https://20.255.48.137";
+export const baseURL = "https://chat-backend.eastasia.cloudapp.azure.com/";
 export const httpClient = axios.create({
   baseURL: baseURL,
 
